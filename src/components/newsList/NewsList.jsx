1040836e9486b971c3ea447b53fe2e95bbb63187
vnelai <118484488+vnelai@users.noreply.github.com>
2025-01-26 14:38:31 -0500

    Build NewsList structure by importing NewsCard and adding the index and article as props to it

--- src/components/newsList/NewsList.jsx
+++ src/components/newsList/NewsList.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import NewsCard from '../newsCard/NewsCard'
 
-function NewsList() {
+function NewsList({newsData}) {
   return (
-    <div>NewsList</div>
-  )
+    <div className='news-list'>
+        {newsData.map((article, index) => (
+            <NewsCard key={index} article={article} /> //Passing article as prop to NewsCard
+        ))}
+    </div>
+  );
 }
 
 export default NewsList
