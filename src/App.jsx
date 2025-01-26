45549fc9916dbea430444419d73288ac3ff288fb
vnelai <118484488+vnelai@users.noreply.github.com>
2025-01-26 13:45:56 -0500

    Import NewsCard component to app.jsx to be able to view the changes on browser

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Header from './components/header/Header'
+import NewsCard from './components/newsCard/NewsCard'
 import './App.css'
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
   return (
     <>
       <Header/>
+      <NewsCard/>
     </>
   )
 }
