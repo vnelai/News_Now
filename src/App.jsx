d05174109cc8508b631972472115fc616e425966
vnelai <118484488+vnelai@users.noreply.github.com>
2025-01-26 13:56:20 -0500

    Create state to hold news data

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
-import Header from './components/header/Header'
-import NewsCard from './components/newsCard/NewsCard'
-import './App.css'
+import React, {useState, useEffect} from 'react'; // Import React modules
+import Header from './components/header/Header'; // Import Header component 
+import NewsCard from './components/newsCard/NewsCard'; // Import NewsCard component
+import './App.css';
 
 function App() {
+  // Create state to hold news data
+  const [newsData, setNewsData] = useState([]);
 
 
   return (
--- src/components/newsCard/NewsCard.jsx
+++ src/components/newsCard/NewsCard.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 function NewsCard() {
   return (
-    <div>NewsCard</div>
+    <div className='news-card'>
+        <img src="" alt="" />
+        <div className='news-card-details'>
+            
+
+        </div>
+
+    </div>
   )
 }
 
