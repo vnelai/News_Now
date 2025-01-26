42d18ea25276f3e6be6a62e7c20f4cf1a37c8476
vnelai <118484488+vnelai@users.noreply.github.com>
2025-01-26 12:38:37 -0500

    Import Header component to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-
+import React from 'react'
+import Header from './components/Header'
 import './App.css'
 
 function App() {
@@ -6,7 +7,7 @@ function App() {
 
   return (
     <>
-      <h1>hello world</h1>
+      <header/>
     </>
   )
 }
